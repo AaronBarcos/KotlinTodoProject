bec8f1bcfcd9153091e6e8f3a81ab5e8d9669e16
Aaron Barcos <aaronbarcos@poloclub.com>
2024-06-03 12:51:36 +0200

    Second Commit

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,13 +1,29 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.Navigator
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.navigator.currentOrThrow
+import cafe.adriel.voyager.transitions.SlideTransition
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -18,18 +34,48 @@ import todoproject.composeapp.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+        Navigator(screen = MainScreen()){navigator: Navigator ->  
+            SlideTransition(navigator)
+        }
+    }
+}
+
+class MainScreen:Screen {
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.current
+        var name:String by remember { mutableStateOf("") }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = name,
+                onValueChange = { name = it }
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            AnimatedVisibility(name.isNotEmpty()) {
+                val displayText:String = if (name == "Aaron") "Bienvenido" else "Hola $name"
+                Text(displayText, fontSize = 24.sp)
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+            Button(onClick = {
+                navigator?.push(SecondScreen())
+            }) {
+                Text("Segunda pantalla")
             }
+        }
+    }
+}
+
+class SecondScreen : Screen {
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+        Column(
+            modifier = Modifier.fillMaxSize().background(Color.Blue),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Segunda pantalla", fontSize = 26.sp, color = Color.White)
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = { navigator.pop() }) { Text("Volver") }
         }
     }
 }
